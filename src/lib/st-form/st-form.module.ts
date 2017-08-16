2e66bde814822a747520133f0376646fad08b6d9
Jose Núñez <jcnunez@stratio.com>
2017-08-16 09:09:31 +0200

    add st-form-label

--- src/lib/st-form/st-form.module.ts
+++ src/lib/st-form/st-form.module.ts
@@ -12,18 +12,28 @@ import { CommonModule }  from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { StFormComponent } from './st-form.component';
-import { StFormFieldComponent } from './st-form-field/st-form-field.component';
 import { PipesModule } from '../pipes/pipes.module';
-import { StInputModule } from '../st-input/st-input.module';
+import { StFormComponent } from './st-form.component';
 import { StFormDirectiveModule } from '../directives/form/form-directives.module';
+import { StFormFieldComponent } from './st-form-field/st-form-field.component';
+import { StInputModule } from '../st-input/st-input.module';
 import { StSwitchModule } from '../st-switch/st-switch.module';
 
 @NgModule({
-   imports: [CommonModule, FormsModule, ReactiveFormsModule, StInputModule, StSwitchModule, PipesModule, StFormDirectiveModule],
-   declarations: [StFormComponent, StFormFieldComponent],
-   exports: [StFormComponent]
+   imports: [
+      CommonModule,
+      FormsModule,
+      PipesModule,
+      ReactiveFormsModule,
+      StFormDirectiveModule,
+      StInputModule,
+      StSwitchModule
+   ],
+   declarations: [
+      StFormComponent,
+      StFormFieldComponent
+   ],
+   exports: [ StFormComponent ]
 })
 
-export class StFormModule {
-}
+export class StFormModule { }
